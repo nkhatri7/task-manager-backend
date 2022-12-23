a66fe388825f5e67acaecf433ba0fbaf93ac2066
nkhatri7 <79176897+nkhatri7@users.noreply.github.com>
2022-12-23 15:50:37 +1200

    renaming register user path

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { createUser, signInUser, checkPasswordMatches } = require('../controllers/auth');
 
-router.route('/create').post(createUser);
+router.route('/register').post(createUser);
 router.route('/login').post(signInUser);
 router.route('/change-password/:id').patch(checkPasswordMatches);
 
