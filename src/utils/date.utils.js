2073aa745bfcad9378aa35a6814057c23c1cd184
nkhatri7 <79176897+nkhatri7@users.noreply.github.com>
2023-01-25 21:18:01 +1100

    updating datetime formatting

--- src/utils/date.utils.js
+++ src/utils/date.utils.js
@@ -1,19 +1,20 @@
+const MONTH_ABBREVIATIONS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct',
+'Nov', 'Dec'];
+
 /**
  * Formats the given date into the following format MMM yyyy.
  * @param {Date} date the date that needs to be formatted
- * @returns {String} A formatted date string.
+ * @returns {string} A formatted date string.
  */
 const getUserAccountCreationDate = (date) => {
-    const monthAbbrevs = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct',
-        'Nov', 'Dec'];
-    const monthName = monthAbbrevs[date.getMonth()];
+    const monthName = MONTH_ABBREVIATIONS[date.getMonth()];
     return `${monthName} ${date.getFullYear()}`;
 };
 
 /**
  * Formats the given date into the following format: dd/MM/yyyy.
  * @param {Date} date the date that needs to be formatted
- * @returns {String} A formatted date string.
+ * @returns {string} A formatted date string.
  */
 const getFormattedDate = (date) => {
     const day = formatNumber(date.getDate());
@@ -23,13 +24,33 @@ const getFormattedDate = (date) => {
 };
 
 /**
- * Formats the given date into the following format: dd/MM/yyyy hh:mm:ss.
+ * Formats the given date into the following format: dd MMM yyyy hh:mm:ss AM/PM.
  * @param {Date} date the date that needs to be formatted
- * @returns {String} A formatted datetime string.
+ * @returns {string} A formatted datetime string.
  */
 const getFormattedDateTime = (date) => {
-    const dateString = getFormattedDate(date);
-    return `${dateString} ${date.toLocaleTimeString()}`;
+    const day = formatNumber(date.getDate());
+    const monthName = MONTH_ABBREVIATIONS[date.getMonth()];
+    const year = date.getFullYear();
+    const timeString = getTimeString(date);
+    return `${day} ${monthName} ${year} ${timeString}`;
+};
+
+/**
+ * Formats the given datetime into the following format hh:mm:ss AM/PM (12 hour time).
+ * @param {Date} date The date that needs to be formatted
+ * @returns {string} A formatted time string.
+ */
+const getTimeString = (date) => {
+    let hour = '';
+    if (date.getHours() > 12) {
+        hour = formatNumber(date.getHours() - 12);
+    } else {
+        hour = formatNumber(date.getHours());
+    }
+    const minute = formatNumber(date.getMinutes());
+    const second = formatNumber(date.getSeconds());
+    return `${hour}:${minute}:${second} ${date.getHours() > 12 ? 'PM' : 'AM'}`;
 };
 
 /**
@@ -38,7 +59,7 @@ const getFormattedDateTime = (date) => {
  * @returns A formatted string of the given number.
  */
 const formatNumber = (num) => {
-    return num < 10 && num > 0 ? `0${num}` : `${num}`;
+    return num < 10 && num >= 0 ? `0${num}` : `${num}`;
 };
 
 module.exports = { 
